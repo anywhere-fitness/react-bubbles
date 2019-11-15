d00580762e5681e2399c8ddc6db6f6fd79828b11
Eric Foster <elitheroyalone@gmail.comgit config --global user.name eric-fostergit config --global user.email elitheroyalone@gmail.com>
2019-11-15 12:59:13 -0600

    MVP

---
+++ client/src/Answer.md
@@ -0,0 +1,19 @@
+Explain what a token is used for.
+
+To store the user state on the client
+
+
+ What steps can you take in your web apps to keep your data secure?
+
+ use protected route and authentication headers
+
+
+ Describe how web servers work.
+
+ It is what renders the web page
+
+
+
+ Which HTTP methods can be mapped to the CRUD acronym that we use when interfacing with APIs/Servers.
+
+ The DELETE HTTP request and PUT
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,50 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-
-  const [creds, setCreds] = useState({username: "", password: ""});
-  const handleChange = event => {
-      setCreds({...creds, [event.target.name]: event.target.value});
-  };
-
-  const handleSubmit = event => {
-    event.preventDefault();
-    axios.post('http://localhost:5000/api/login', creds)
-    .then(res => {console.log(res);
-        localStorage.setItem('token', res.data.payload);
-        props.history.push('/BubblePage');
-    })
-    .catch(err => console.log(err.response));
+const initialState = {
+  username: "",
+  password: ""
 }
+const Login = (props) => {
+  const [login, setLogin] = useState(initialState)
 
+  const handleChanges = e => {
+    setLogin({ ...login, [e.target.name] : e.target.value})
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    axiosWithAuth()
+      .post("./login", login)
+      .then(res => {
+        console.log(res)
+        localStorage.setItem('token', res.data.payload)
+        props.history.push('/bubbles');
+    })
+    setLogin(initialState)
+  }
 
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      {/* <p>Build a login page here</p> */}
-
       <form onSubmit={handleSubmit}>
-
-      <input type='text'
-      name='username'
-      placeholder='username' 
-      onChange={handleChange} 
-      value={creds.username}/>
-
-      <input type='password' 
-      name='password' 
-      placeholder='password' 
-      onChange={handleChange} 
-      value={creds.password}/>
-
-      <button type='submit'>Submit</button>
-
-    </form>
-
+        <input 
+          type="text"
+          name="username"
+          placeholder="username"
+          onChange={handleChanges}
+          value={login.username}
+        />
+        <input 
+          type="password"
+          name="password"
+          placeholder="password"
+          onChange={handleChanges}
+          value={login.password}
+        />
+        <button type="submit">Login</button>
+      </form>
     </>
   );
 };
 
 export default Login;
+
